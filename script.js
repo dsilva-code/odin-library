79e7271439d55b345f136d8b34af944810260005
grillgrut <grillgrut@gmail.com>
2026-07-24 10:23:28 -0500

    Add inputs for new book button with dialog

--- script.js
+++ script.js
@@ -16,13 +16,23 @@ function addBookToLibrary (title, author, pages, read) {
 }
 
 function libraryInfo () {
+    const pageBody = document.getElementById("library");
 
-    
     for (let i = 0; i < myLibrary.length; i++) {
-        
-        console.log(myLibrary[i].title + ", " + myLibrary[i].author + ", " + myLibrary[i].pages + ", " + myLibrary[i].read + ", " + myLibrary[i].id);
+        const newBook = document.createElement('div');
+        newBook.innerHTML = "Title: " + myLibrary[i].title + "<br>Author: " + myLibrary[i].author + "<br>Pages: " + myLibrary[i].pages + "<br>Read: " + myLibrary[i].read + "<br>ID: " + myLibrary[i].id + "<br><br>";
+        pageBody.appendChild(newBook);
     }
 }
+
+const submit = document.getElementById("bookSubmit");
+submit.onclick = function() {
+    preventDefault();
+};
+
+const bookTitle = document.getElementById("bookTitle");
+
+
 
 const myLibrary = [];
 
@@ -30,8 +40,5 @@ addBookToLibrary("Project Hail Mary", "Andy Weir", 290, false);
 addBookToLibrary("Stoner", "John Williams", 140, true);
 addBookToLibrary("A Fig for all Devils", "C.S Fritz", 90, true);
 
-    const pageBody = document.getElementById("library");
-
-    
 libraryInfo();
 
