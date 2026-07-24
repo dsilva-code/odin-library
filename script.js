417446e26e60275e05931b90b7932e2b44e9108a
grillgrut <grillgrut@gmail.com>
2026-07-24 08:46:12 -0500

    Create the function to add books to library

--- script.js
+++ script.js
@@ -3,18 +3,22 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = self.crypto.randomUUID();
 
     this.info = function() {
-        console.log(this.title + ", " + this.author + ", " + this.pages + ", " + this.read)
+        console.log(this.title + ", " + this.author + ", " + this.pages + ", " + this.read + ", " + this.id)
     }
 }
 
+function addBookToLibrary (title, author, pages, read) {
+    book1 = new Book(title, author, pages, read);
+    return book1;
+}
+
+const myLibrary = [];
+
 const projectHailMary = new Book("Project Hail Mary", "Andy Weir", 290, false);
+const stoner = new Book("Stoner", "John Williams", 140, true);
 
-projectHailMary.info();
 
-const stoner = new Book("Stoner", "John Williams", 140, true)
-
-stoner.info();
-
-console.log(Object.getPrototypeOf(projectHailMary) === Book.prototype)
+console.log(addBookToLibrary("YO","YO", 100, false))
