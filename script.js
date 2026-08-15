3c7052ce320a4714ce3a429c5d327e2a9ad80a84
grillgrut <grillgrut@gmail.com>
2026-08-15 09:28:01 -0500

    Change the add button to fix from previous error

--- script.js
+++ script.js
@@ -22,71 +22,49 @@ class newLibrary {
     myLibrary = [];
 
     addBook(title, author, page, read) {
-        book1 = new newBook(title, author, page, read);
-        myLibrary.push(book1);
+        let book1 = new newBook(title, author, page, read);
+        this.myLibrary.push(book1);
     }
+
+    showBooks() {
+        const body = document.getElementById("body");
+        const  oldPageBody = document.getElementById("library");
+        oldPageBody.remove();
+        
+        const pageBody = document.createElement("div");
+        pageBody.id = "library";
+        body.appendChild(pageBody);
+        
+        for (let i = 0; i < this.myLibrary.length; i++) {
+            const newBook = document.createElement('div');
+            newBook.innerHTML = "<br>Title: " + this.myLibrary[i].title + "<br>Author: " + this.myLibrary[i].author + "<br>Pages: " + this.myLibrary[i].pages + "<br>Read: " + this.myLibrary[i].read + "<br>ID: " + this.myLibrary[i].id + "<br>";
+            newBook.dataset.id = this.myLibrary[i].id;
+            pageBody.appendChild(newBook);
+        }
+
+        const books = document.querySelectorAll("#library div");
+        
+        for (const element of books) {
+            const removeButton = document.createElement("button");
+            removeButton.className = "removeButtons";
+            removeButton.textContent = "Remove Book";
+            element.appendChild(removeButton);
+            const readButton = document.createElement("button");
+            readButton.className = "readButtons";
+            readButton.textContent = "Read Status";
+            element.appendChild(readButton);
+        }
+    }
+    
 }
 
-
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = self.crypto.randomUUID();
-
-    this.info = function() {
-        console.log(this.title + ", " + this.author + ", " + this.pages + ", " + this.read + ", " + this.id)
-    }
-}
+/*
 
 Book.prototype.changeRead = function() {
    this.read = !this.read;
 }
 
-function addBookToLibrary (title, author, pages, read) {
-    book1 = new Book(title, author, pages, read);
-    myLibrary.push(book1);
-}
-
-function libraryInfo () {
-    const body = document.getElementById("body");
-    const  oldPageBody = document.getElementById("library");
-    oldPageBody.remove();
-    
-    const pageBody = document.createElement("div");
-    pageBody.id = "library";
-    body.appendChild(pageBody);
-    
-    for (let i = 0; i < myLibrary.length; i++) {
-        const newBook = document.createElement('div');
-        newBook.innerHTML = "<br>Title: " + myLibrary[i].title + "<br>Author: " + myLibrary[i].author + "<br>Pages: " + myLibrary[i].pages + "<br>Read: " + myLibrary[i].read + "<br>ID: " + myLibrary[i].id + "<br>";
-        newBook.dataset.id = myLibrary[i].id;
-        pageBody.appendChild(newBook);
-    }
-
-    const books = document.querySelectorAll("#library div");
-    
-    for (const element of books) {
-        const removeButton = document.createElement("button");
-        removeButton.className = "removeButtons";
-        removeButton.textContent = "Remove Book";
-        element.appendChild(removeButton);
-        const readButton = document.createElement("button");
-        readButton.className = "readButtons";
-        readButton.textContent = "Read Status";
-        element.appendChild(readButton);
-    }
-}
-
-
-
-const myLibrary = [];
-
-addBookToLibrary("Project Hail Mary", "Andy Weir", 290, false);
-addBookToLibrary("Stoner", "John Williams", 140, true);
-addBookToLibrary("A Fig for all Devils", "C.S Fritz", 90, true);
+*/
 
 const submit = document.getElementById("bookSubmit");
 const dialog = document.getElementById("my-dialog");
@@ -104,8 +82,8 @@ function submitClick(event) {
     const author = bookAuthor.value;
     const pages = bookPages.value;
     let read = bookRead.checked;
-    addBookToLibrary (title, author, pages, read);
-    libraryInfo();
+    theLibrary.addBook(title, author, pages, read);
+    theLibrary.showBooks();
 
     const removeButtons = document.querySelectorAll(".removeButtons");
 
@@ -119,9 +97,6 @@ function submitClick(event) {
         element.addEventListener("click", readClick);
     }
 }
-
-
-libraryInfo();
 
 const removeButtons = document.querySelectorAll(".removeButtons");
 
@@ -186,5 +161,7 @@ function readClick(event) {
 
 const theLibrary = new newLibrary();
 theLibrary.addBook("Land of the Lustrous", "Akira", 888, true);
-
+theLibrary.addBook("Project Hail Mary", "Andy Weir", 472, true);
+console.log(theLibrary);
+theLibrary.showBooks()
 
